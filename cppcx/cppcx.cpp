7434f4b794d8a2ee367e82e3b7e23882d19244aa
Lyroy TheToad <lyroy.thetoad@gmail.com>
2022-07-15 14:02:40 +0200

    Small change in `cx::Future` destructor

--- cppcx/cppcx.cpp
+++ cppcx/cppcx.cpp
@@ -15,7 +15,7 @@ cx::Future::Future() noexcept {}
 
 cx::Future::~Future()
 {
-    if (mpKeepRunning) *mpKeepRunning = false;
+    GiveUp();
 
     // There is no need to call `mFuture.wait()` since its deconstructor already does it
 }
